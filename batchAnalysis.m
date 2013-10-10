ddd2d3aaf882f703ead6331a7836cd2e3059e2be
Geoffrey Jones <jonesg5@rpi.edu>
2013-10-10 12:07:11 -0400

    Added support functions

--- batchAnalysis.m
+++ batchAnalysis.m
@@ -2,16 +2,19 @@ function batchAnalysis
 %BATCHANALYSIS Summary of this function goes here
 %   Detailed explanation goes here
 
+addpath('IO');
+
 %% File handling
-projectFolder = fullfile([filesep,filsep],'root','projects',...
-    'NIH Alzheimers','TroyALZ2011');
-rawFolder = fullfile(projectFolder,'rawData');
-indexPath = fullfile(rawFolder,'index.xlsx');
+projectFolder = fullfile([filesep,filesep],'root','projects',...
+    'NIH Alzheimers','AnnaLokData');
+indexPath = fullfile(projectFolder,'index.xlsx');
+textFolder = fullfile(projectFolder,'textData');
 cdfFolder = fullfile(projectFolder,'cdfData');
+resultsFolder = fullfile(projectFolder,'results');
 
 % Import the index
 [subject,AIM,dimeSN,dimeFile,startTime,stopTime,bedTime,wakeTime] = ...
-    importIndex(file);
+    importIndex(indexPath);
 
 end
 
